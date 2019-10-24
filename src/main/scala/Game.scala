ddea9e513c2d97fd2e10d36a322fc40da7e70b83
vimikl <vincent.mikl@htwg-konstanz.de>
2019-10-24 11:49:40 +0200

    Vincent contribute test

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -4,6 +4,9 @@ object Game {
   def main(args: Array[String]): Unit = {
     val player = Player("Önder")
     println("Hello, "+player.name)
+
+    val player2 = Player("Vincent")
+    println("Hello, "+player.name)
   }
 }
 
