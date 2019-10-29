ae8c13078ff4bb59e6918277462e351804fb927f
OenderTuetuenci <oendertuetuenci@gmx.de>
2019-10-29 10:59:21 +0100

    added functions in feld for buying and selling homes

--- src/main/scala/model/Feld.scala
+++ src/main/scala/model/Feld.scala
@@ -9,17 +9,29 @@ abstract class Feld(name: String,number: Int){
   override def toString: String = name + ' ' + number
 }
 
-case class Strasse(name: String,number: Int,price: Int,owner: String,rent: Int) extends Feld(name,number) {
+case class Strasse(name: String,number: Int,price: Int,owner: String,rent: Int,home:Int) extends Feld(name,number) {
   override def onPlayerEntered(): String = {
     println("\nplayer entered " + this.name + ". owner: " + this.owner)
     if (this.owner == "") "buy"
     else "pay"
 
   }
-  def setOwner(x:String) : Strasse = Strasse(name,number,price,x,rent)
+  def setOwner(x:String) : Strasse = Strasse(name,number,price,x,rent,home)
+  //Functions to buy or sell homes to increase rent
+  def buyHome(x:Int) : Strasse ={
+    val newRent = rent+(home * 200)
+    Strasse(name,number,price,owner,newRent,home+x)
+  }
+  def sellHome(x:Int) : Strasse={
+    val newRent = rent-(x*200)
+    Strasse(name,number,price,owner,newRent,home-x)
+  }
 
   override def toString: String = {
-    name + ' ' + number + ' ' + price + ' ' + rent + " Owner: " + owner
+    if(home > 0)
+      name + ' ' + number + ' ' + price + ' ' + rent + " Owner: " + owner +" homecount: "+home
+    else
+      name + ' ' + number + ' ' + price + ' ' + rent + " Owner: " + owner
   }
 
 }
