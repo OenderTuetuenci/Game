db0f744c5a394b511c1a9779396f96080ceb094e
OenderTuetuenci <oender.tuetuenci@gmx.de>
2019-11-21 12:50:45 +0100

    changed Tui update so it would be testable

--- src/main/scala/view/Tui.scala
+++ src/main/scala/view/Tui.scala
@@ -9,35 +9,40 @@ import scala.io.StdIn._
 class Tui(controller: Controller) extends Observer {
     controller.add(this)
     def getController : Controller = controller
-    override def update(e: Event) = {
+    override def update(e: Event):Boolean = {
+      var worked = false;
+      var string = "";
         e match {
-            case e: brokeEvent => println(getBrokeEventString(e))
-            case e: gameOverEvent => println(getGameOverString(e))
-            case e: payRentEvent => println(getPayRentString(e))
-            case e: buyStreetEvent => println(getBuyStreetEventString(e))
-            case e: buyTrainstationEvent => println(getBuyTrainstationEventString(e))
-            case e: playerInJailEvent => println(getPlayerInJailString(e))
-            case e: normalTurnEvent => println(getNormalTurnString(e))
-            case e: diceEvent => println(getRollString(e))
-            case e: playerSellsStreetEvent => println(getPlayerSellsStreetString(e))
-            case e: playerUsesHyptohekOnStreetEvent => println(getPlayerUsesHypothekOnStreetString(e))
-            case e: playerPaysHyptohekOnStreetEvent => println(getPlayerPaysHypothekOnStreetString(e))
-            case e: playerUsesHyptohekOnTrainstationEvent => println(getPlayerUsesHypothekOnTrainstationString(e))
-            case e: playerPaysHyptohekOnTrainstationEvent => println(getPlayerPaysHypothekOnTrainstationString(e))
-            case e: playerSellsTrainstationEvent => println(getPlayerSellsTrainstationString(e))
-            case e: newRoundEvent => println(getNewRoundString(e))
-            case e: endRoundEvent => println(getEndRoundString(e))
-            case e: playerMoveToJail => println(getPlayerMoveToJailString(e))
-            case e: optionEvent => println(getOptionString(e))
-            case e: printEverythingEvent => println(getPlayerAndBoardToString)
-            case e: playerMoveEvent => println(getPlayerMovedString(e))
-            case e: playerIsFreeEvent => println(getPlayerIsFreeString(e))
-            case e: playerRemainsInJailEvent => println(getPlayerRemainsInJailString(e))
-            case e: playerWentOverGoEvent => println(getPlayerWentOverGoString(e))
-            case e: playerWentOnGoEvent => println(getPlayerWentOnGoString(e))
-            case e: streetOnHypothekEvent => println(getStreetOnHypothekString(e))
-            case e: playerHasDeptEvent => println(getPlayerHasDeptEventString(e))
+          case e: brokeEvent => string = getBrokeEventString(e);worked = true
+          case e: gameOverEvent => string = getGameOverString(e);worked = true
+          case e: payRentEvent => string = getPayRentString(e);worked = true
+          case e: buyStreetEvent => string = getBuyStreetEventString(e);worked = true
+          case e: buyTrainstationEvent => string = getBuyTrainstationEventString(e);worked = true
+          case e: playerInJailEvent => string = getPlayerInJailString(e);worked = true
+          case e: normalTurnEvent => string = getNormalTurnString(e);worked = true
+          case e: diceEvent => string = getRollString(e);worked = true
+          case e: playerSellsStreetEvent => string = getPlayerSellsStreetString(e);worked = true
+          case e: playerUsesHyptohekOnStreetEvent => string = getPlayerUsesHypothekOnStreetString(e);worked = true
+          case e: playerPaysHyptohekOnStreetEvent => string = getPlayerPaysHypothekOnStreetString(e);worked = true
+          case e: playerUsesHyptohekOnTrainstationEvent => string = getPlayerUsesHypothekOnTrainstationString(e);worked = true
+          case e: playerPaysHyptohekOnTrainstationEvent => string = getPlayerPaysHypothekOnTrainstationString(e);worked = true
+          case e: playerSellsTrainstationEvent => string = getPlayerSellsTrainstationString(e);worked = true
+          case e: newRoundEvent => string = getNewRoundString(e);worked = true
+          case e: endRoundEvent => string = getEndRoundString(e);worked = true
+          case e: playerMoveToJail => string = getPlayerMoveToJailString(e);worked = true
+          case e: optionEvent => string = getOptionString(e);worked = true
+          case e: printEverythingEvent => string = getPlayerAndBoardToString;worked = true
+          case e: playerMoveEvent => string = getPlayerMovedString(e);worked = true
+          case e: playerIsFreeEvent => string = getPlayerIsFreeString(e);worked = true
+          case e: playerRemainsInJailEvent => string = getPlayerRemainsInJailString(e);worked = true
+          case e: playerWentOverGoEvent => string = getPlayerWentOverGoString(e);worked = true
+          case e: playerWentOnGoEvent => string = getPlayerWentOnGoString(e);worked = true
+          case e: streetOnHypothekEvent => string = getStreetOnHypothekString(e);worked = true
+          case e: playerHasDeptEvent => string = getPlayerHasDeptEventString(e);worked = true
+          case _ => worked = false
         }
+            println(string)
+            worked
     }
 
     def getPlayerCount: Unit = {
