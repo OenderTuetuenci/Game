e68ae73f8bc9f2d6192af0aa84bf12d3996d5708
OenderTuetuenci <oender.tuetuenci@gmx.de>
2019-11-21 14:02:13 +0100

    created ControllerTest

---
+++ src/test/scala/ControllerTest.scala
@@ -0,0 +1,10 @@
+import controller.Controller
+import org.scalatest.{Matchers, WordSpec}
+
+class ControllerTest extends WordSpec with Matchers{ {
+  "A Controller" when {
+    val controller = new Controller()
+
+  }
+}
+}
